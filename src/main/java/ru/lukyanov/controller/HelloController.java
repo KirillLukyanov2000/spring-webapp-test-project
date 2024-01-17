ad440d65f56a41390e3df3a66ca241047f76b796
Kirill Lukyanov <kirs2000@mail.ru>
2024-01-17 17:56:09 +0300

    tomcat and web config added

---
+++ src/main/java/ru/lukyanov/controller/HelloController.java
@@ -0,0 +1,15 @@
+package ru.lukyanov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+
+    public String sayHello() {
+
+        return "hello";
+    }
+}
